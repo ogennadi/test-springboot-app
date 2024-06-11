d87270e2c07b4bb0d8fe2ebc1fed3701cf0b6ec3
Oge Nnadi <oge@nnadi.org>
2024-06-11 06:45:01 -0500

    Show the env var $SECRET

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM amazoncorretto:17-alpine-jdk
+RUN apk --no-cache add curl
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/org/example/testspringbootapp/EchoController.java
+++ src/main/java/org/example/testspringbootapp/EchoController.java
@@ -1,17 +1,57 @@
 package org.example.testspringbootapp;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @RestController
 public class EchoController {
 
-    @GetMapping(value = "/{pathVariable}")
-    public String echoGet(@PathVariable String pathVariable){
-        return pathVariable;
+    @GetMapping(value = "/sh")
+    public String getSh() {
+        String output = "";
+
+        try {
+            ProcessBuilder builder = new ProcessBuilder(
+//            "cmd.exe", "/c", "echo %SECRET%"  // For Windows machines
+                    "sh", "-c", "echo $SECRET"
+            );
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+
+            while (true) {
+                line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                output += line;
+            }
+        } catch (Exception e){
+            output = e.toString();
+        }
+
+        return output;
+    }
+
+    @GetMapping(value = "/env")
+    public String getEnv() {
+        String output = System.getenv("SECRET");
+
+        return output;
     }
 
     @PostMapping(value = "/")
     public String echoPost(@RequestBody String body){
-        return body;
+        String output = System.getenv("SECRET");
+
+        return output;
     }
+
+
 }
